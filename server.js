6e454793ddd5e7b01e9e6f608dcb7d99612e990e
Gaël MARIN <mrn.gael@gmail.com>
2018-04-17 11:58:38 +0200

    Added anime.js
    Created sass src directory
    
    Updated index.ejs links and script src

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+})
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
